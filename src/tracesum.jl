2e5abd437456dfbf8adc1a7257dd3b76059babe9
Hua Zhou <huazhou@ucla.edu>
2020-06-09 17:21:01 -0700

    compact way to generate rectangular I

--- src/tracesum.jl
+++ src/tracesum.jl
@@ -486,13 +486,7 @@ function init_eye(
     S :: Matrix{Matrix{T}},
     r :: Integer
     ) where T <: BlasReal
-    O = [zeros(T, size(S[i, i], 1), r) for i in 1:size(S, 1)]
-    for Oi in O
-        for i in 1:r
-            Oi[i, i] = 1
-        end
-    end
-    O
+    [Matrix{T}(I, size(S[i, i], 1), r) for i in 1:size(S, 1)]
 end
 
 """
