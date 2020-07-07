8a372c1da91350170c480c0ce9b6af1432dc7800
Hua Zhou <huazhou@ucla.edu>
2020-07-07 16:50:23 -0700

    put back SDP testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -55,47 +55,50 @@ end
 # display(bm); println()
 end
 
-# @testset "SDP relaxation (port wine example)" begin
-# A, S, ts_optim = portwine_data()
-# r = 1
-# for r in 1:3
-#     for solver in [
-#         #Mosek.Optimizer(LOG=0),
-#         COSMO.Optimizer(max_iter=5000, verbose=false),
-#         SCS.Optimizer(verbose=0)
-#         ]
-#         @info "rank = $r, solver = $solver"
-#         @time Ô_sdp, ts_sdp, = otsm_sdp(S, r, solver = solver)
-#         @show ts_sdp
-#         @test ts_sdp ≈ ts_optim[r]
-#         @test test_optimality(Ô_sdp, S)[1] == 1
-#     end
-# end
-# end
+@testset "SDP relaxation (port wine example)" begin
+A, maxdiff_optim, _ = portwine_data()
+Smaxdiff = [A[i]'A[j] for i in 1:4, j in 1:4]
+for i in 1:4
+    fill!(Smaxdiff[i, i], 0)
+end
+for r in 1:3
+    for solver in [
+        #Mosek.Optimizer(LOG=0),
+        COSMO.Optimizer(max_iter=5000, verbose=false),
+        SCS.Optimizer(verbose=0)
+        ]
+        @info "rank = $r, solver = $solver"
+        @time Ô_sdp, ts_sdp, = otsm_sdp(Smaxdiff, r, solver = solver)
+        @show ts_sdp
+        @test ts_sdp ≈ maxdiff_optim[r]
+        @test test_optimality(Ô_sdp, Smaxdiff)[1] == 1
+    end
+end
+end
 
-# @testset "Procrustes (random data)" begin
-# Random.seed!(123)
-# n, d, m = 10, 3, 50
-# A, S, Ā, A_manopt = generate_procrustes_data(m, n, d)
-# @info "Proximal block ascent algorithm:"
-# for init_fun in [init_eye, init_sb, init_tb]        
-#     @info "init = $init_fun"
-#     @time Ô_ba, ts_ba, = otsm_pba(S, d; verbose = true, O = init_fun(S, d))
-#     @show ts_ba
-#     @test test_optimality(Ô_ba, S)[1] == 1
-# end
-# @info "SDP relaxation (failed to find global solution):"
-# for solver in [
-#     #Mosek.Optimizer(LOG=0),
-#     COSMO.Optimizer(max_iter=5000, verbose=false),
-#     SCS.Optimizer(verbose=0)
-#     ]
-#     @info "solver = $solver"
-#     @time Ô_sdp, ts_sdp, = otsm_sdp(S, d, solver = solver)
-#     @show ts_sdp
-#     @test test_optimality(Ô_sdp, S)[1] == -1
-# end
-# end
+@testset "Procrustes (random data)" begin
+Random.seed!(123)
+n, d, m = 10, 3, 50
+A, S, Ā, A_manopt = generate_procrustes_data(m, n, d)
+@info "Proximal block ascent algorithm:"
+for init_fun in [init_eye, init_sb, init_tb]        
+    @info "init = $init_fun"
+    @time Ô_ba, ts_ba, = otsm_pba(S, d; verbose = true, O = init_fun(S, d))
+    @show ts_ba
+    @test test_optimality(Ô_ba, S)[1] == 1
+end
+@info "SDP relaxation (failed to find global solution):"
+for solver in [
+    #Mosek.Optimizer(LOG=0),
+    COSMO.Optimizer(max_iter=5000, verbose=false),
+    SCS.Optimizer(verbose=0)
+    ]
+    @info "solver = $solver"
+    @time Ô_sdp, ts_sdp, = otsm_sdp(S, d, solver = solver)
+    @show ts_sdp
+    @test test_optimality(Ô_sdp, S)[1] == -1
+end
+end
 
 @testset "MaxBet (Liu-Wang-Wang Example 5.1)" begin
 A = [4.3299 2.3230 -1.3711 -0.0084 -0.7414;
